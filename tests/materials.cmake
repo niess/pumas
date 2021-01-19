6417ea3479c12fccc77770e74df01d109d3143cf
Valentin Niess <niess@in2p3.fr>
2021-01-19 16:12:38 +0100

    Update git tag for materials

--- tests/materials.cmake
+++ tests/materials.cmake
@@ -4,6 +4,7 @@ set (MATERIALS_DIR "${CMAKE_BINARY_DIR}/materials")
 
 ExternalProject_Add (Materials
     GIT_REPOSITORY "https://github.com/niess/pumas-materials"
+    GIT_TAG "f26020c"
     SOURCE_DIR "${CMAKE_BINARY_DIR}/materials"
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
