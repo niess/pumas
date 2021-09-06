86b3957d2eb5972bb66c878807307e316c62761d
Valentin Niess <niess@in2p3.fr>
2021-09-06 14:11:06 +0200

    Clean CMake build options

--- tests/test-pumas.cmake
+++ tests/test-pumas.cmake
@@ -1,62 +1,63 @@
-set (CMAKE_C_FLAGS_TEST
-        "${CMAKE_C_FLAGS_DEBUG} --coverage" CACHE STRING
-        "Flags used by the C compiler during test builds." FORCE)
-mark_as_advanced(CMAKE_C_FLAGS_TEST)
+if (${CMAKE_BUILD_TYPE} MATCHES "Debug")
+        target_compile_options(pumas PUBLIC "--coverage")
+        if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.13)
+                target_link_options(pumas PUBLIC --coverage)
+        else()
+                target_link_libraries(pumas PUBLIC --coverage)
+        endif()
+endif ()
 
 include ("tests/libcheck.cmake")
 
 add_executable (test-pumas EXCLUDE_FROM_ALL "tests/test-pumas.c")
-target_compile_definitions (test-pumas PRIVATE -DPUMAS_VERSION=${PUMAS_VERSION_MAJOR}.${PUMAS_VERSION_MINOR})
+target_compile_definitions (test-pumas PRIVATE
+        -DPUMAS_VERSION=${PUMAS_VERSION_MAJOR}.${PUMAS_VERSION_MINOR})
 add_dependencies (test-pumas LibCheck)
 target_link_libraries (test-pumas check pumas)
 
 add_custom_command(
-    TARGET test-pumas POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E copy
-            ${CMAKE_SOURCE_DIR}/examples/data/materials.xml
-            ${CMAKE_CURRENT_BINARY_DIR}/materials/materials.xml)
+        TARGET test-pumas POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E copy
+                ${CMAKE_SOURCE_DIR}/examples/data/materials.xml
+                ${CMAKE_CURRENT_BINARY_DIR}/materials/materials.xml)
 
 add_custom_command(
-    TARGET test-pumas POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E make_directory
-            ${CMAKE_CURRENT_BINARY_DIR}/materials/dedx/muon)
+        TARGET test-pumas POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E make_directory
+                ${CMAKE_CURRENT_BINARY_DIR}/materials/dedx/muon)
 
 add_custom_command(
-    TARGET test-pumas POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E make_directory
-            ${CMAKE_CURRENT_BINARY_DIR}/materials/dedx/tau)
-
-if (${CMAKE_BUILD_TYPE} MATCHES Test)
-        set (__args "coverage")
-else ()
-        set (__args "")
-endif ()
+        TARGET test-pumas POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E make_directory
+                ${CMAKE_CURRENT_BINARY_DIR}/materials/dedx/tau)
 
 if (${CMAKE_VERSION} VERSION_GREATER "3.0.0")
-    cmake_policy (SET CMP0037 OLD)
+        cmake_policy (SET CMP0037 OLD)
 endif ()
 
-if (${CMAKE_BUILD_TYPE} MATCHES "Test")
-    # Add a target for test(s)
-    add_custom_target (test DEPENDS test-pumas
-        COMMAND lcov --directory . --zerocounters
+# Add a target for test(s)
+add_custom_target (test DEPENDS test-pumas
         COMMAND test-pumas
-        COMMAND lcov --directory . --capture --output-file coverage.info
-        COMMAND lcov --remove coverage.info '*/tests/*' '*/include/*' '/usr/*' --output-file coverage.info
-        COMMAND lcov --list coverage.info
 
         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
         COMMENT "Running test(s)")
-else ()
-    add_custom_target (test DEPENDS test-pumas
-        COMMAND test-pumas ${__args}
 
-        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-        COMMENT "Running test(s)")
+if (${CMAKE_BUILD_TYPE} MATCHES "Debug")
+        # Add a target for coverage (requires lcov)
+        add_custom_target (coverage DEPENDS test-pumas
+                COMMAND lcov --directory . --zerocounters
+                COMMAND test-pumas
+                COMMAND lcov --directory . --capture --output-file coverage.info
+                COMMAND lcov --remove coverage.info '*/tests/*' '*/include/*' '/usr/*' --output-file coverage.info
+                COMMAND lcov --list coverage.info
+
+                WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+                COMMENT "Running coverage")
 endif ()
 
+# Add a target for testing memory usage (requires valgrind)
 add_custom_target (memcheck DEPENDS test-pumas
         COMMAND CK_NOFORK=on valgrind --error-exitcode=1 --leak-check=full --show-reachable=yes ./test-pumas
 
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-    COMMENT "Running memory check")
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+        COMMENT "Running memory check")
